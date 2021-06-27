36f672af24b244349f184fb3cc9a4a316ad50ad2
xuzewei02 <haianxzw3@163.com>
2021-06-27 17:02:47 +0800

    安装部分插件，删除初始化部分hello word 页面

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//element-ui
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+
+//axios
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
+//mockjs
+
+
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
