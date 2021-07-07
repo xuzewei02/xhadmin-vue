25f8fa4d3eb7f4c919934405498589e0a6cd926e
xuzewei02 <haianxzw3@163.com>
2021-07-07 20:04:30 +0800

    换个打包方式

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,8 @@ const port = 9001 // 端口配置
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   // hash 模式下可使用
-  // publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
+  // publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
